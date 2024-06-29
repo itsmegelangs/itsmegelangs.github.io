18a801fc8a4b1f285a2c98c43dca637d677d9725
itsmegelangs <torioricardo356@gmail.com>
2024-06-29 19:06:43 +0800

    Update app.js

--- app.js
+++ app.js
@@ -6,31 +6,20 @@ var product2 = document.getElementById("product2")
 var qty2 = document.getElementById("qty2")
 var price2 = document.getElemnetById("price2")
 
-var product3 = document.getElementById("product3")
-var qty3 = document.getElementById("qty3")
-var price3 = document.getElemnetById("price3")
-
-var product4 = document.getElementById("product4")
-var qty4 = document.getElementById("qty4")
-var price4 = document.getElemnetById("price4")
-
-var product5 = document.getElementById("product5")
-var qty5 = document.getElementById("qty5")
-var price5 = document.getElemnetById("price5")
-
-var product6 = document.getElementById("product6")
-var qty6 = document.getElementById("qty6")
-var price6 = document.getElemnetById("price6")
-
-function addOrder () {
+function addOrder() {
   carts.textContent=""
   if (parseFloat (qty1.value) >  0) {
-    var order= qty1.value.toString() + 'pc/s x '+ price1.textContent + '------'+ product1.textContent + '------ Php' + (parseFloat(qty1.value)*parseFloat(price1.textContent)) + '\n'
+    var order= qty1.value.toString() + ' pc/s x '+ price1.textContent + '------'+ product1.textContent + '------ Php' + (parseFloat(qty1.value)*parseFloat(price1.textContent)) + '\n'
+    //carts.textContent += carts.value.toString() + "\n";
+    carts.textContent += order
+  }
+   if (parseFloat (qty2.value) >  0) {
+    var order= qty2.value.toString() + ' pc/s x '+ price2.textContent + '------'+ product2.textContent + '------ Php' + (parseFloat(qty2.value)*parseFloat(price2.textContent)) + '\n'
     //carts.textContent += carts.value.toString() + "\n";
     carts.textContent += order
   }
 }
-  qty1.addEventListener("keyup", addOrder) ;
-  
+  qty1.addEventListener("keyup", addOrder);
+  qty2.addEventLIstener("keyup", addOrder);
     
 
