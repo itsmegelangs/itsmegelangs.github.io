5614cd7ba7a0739e0662ef83478b2447fd9e2352
itsmegelangs <torioricardo356@gmail.com>
2024-07-04 19:29:58 +0800

    Update app.js

--- app.js
+++ app.js
@@ -33,8 +33,9 @@ var change= document.getElementById("change")
 
 function addOrder() {
   carts.textContent=""
+  totalCost = 0
   if (parseFloat (qty1.value) >  0) {
-    var order= qty1.value.toString() + ' pc/s x '+ price1.textContent + '------'+ product1.textContent + '------ Php' + (parseFloat(qty1.value)*parseFloat(price1.textContent)) + '\n'
+    var order= qty1.value.toString() + ' pc/s x '+ price1.textContent + '------'+ product1.textContent + '------ Php' + (parseFloat(qty1.value)*parseFloat(price1.textContent)) + float(totalCost) * int(qty1) '\n'
     //carts.textContent += carts.value.toString() + "\n";
     carts.textContent += order
   }
