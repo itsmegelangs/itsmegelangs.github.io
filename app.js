f541a64b442626e50dca92d34e76ae88b8f68c25
itsmegelangs <torioricardo356@gmail.com>
2024-07-06 06:14:12 +0800

    Update app.js

--- app.js
+++ app.js
@@ -40,6 +40,7 @@ function addOrder() {
     //carts.textContent += carts.value.toString() + "\n";
     carts.textContent += order
     totalCost += orderCost;
+     total.textContent = `Total: Php ${totalCost.toFixed(2)}`;
   }
    if (parseFloat (qty2.value) >  0) {
     var order= qty2.value.toString() + ' pc/s x '+ price2.textContent + '------'+ product2.textContent + '------ Php' + (parseFloat(qty2.value)*parseFloat(price2.textContent)) + '\n'
